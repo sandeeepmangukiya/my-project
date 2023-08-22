41ea1edbdad9b5dab6f1e6d6042efbee0b17d588
sandeeepmangukiya <s.mangukiya@web.de>
2023-08-22 16:16:55 +0200

    added initial Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+
+        System.out.println("The Multiplication is " + (a*b));
+    }
+}
