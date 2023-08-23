c995a51d277ad3d9645c26911a602c8db0962cc6
sandeeepmangukiya <s.mangukiya@web.de>
2023-08-23 14:00:49 +0200

    added Main.java initially

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Main{
+    public static void main(String[] args) {
+        HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+        capitalCities.put("England", "London");
+        capitalCities.put("India", "New Delhi");
+        capitalCities.put("Germany", "Berlin");
+        capitalCities.put("USA", "Washington DC");
+
+        System.out.println(capitalCities);
+    }
+}
