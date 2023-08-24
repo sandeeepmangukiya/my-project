14aa4922dbd75986a7e0d101874ce5de5f9bc4d1
sandeeepmangukiya <s.mangukiya@web.de>
2023-08-24 12:05:59 +0200

    added Vehicle as an example of Inheritance

---
+++ Vehicle/Vehicle.java
@@ -0,0 +1,17 @@
+package Vehicle;
+public class Vehicle {
+    protected String brand = "Ford";
+    public void honk(){
+        System.out.println("Tuut, Tuut!!");
+    }
+}
+
+class Car extends Vehicle{
+    private String modelName = "Mustang";
+    public static void main(String[] args) {
+        Car myCar = new Car();
+        myCar.honk();
+
+        System.out.println(myCar.brand + " " + myCar.modelName);
+    }
+}
