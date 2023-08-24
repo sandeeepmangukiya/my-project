45bfb863c040163ab0330f8357b11f96f7aa2a36
sandeeepmangukiya <s.mangukiya@web.de>
2023-08-24 15:06:24 +0200

    modified FirstScanner.java

--- FirstScanner.java
+++ FirstScanner.java
@@ -3,9 +3,13 @@ import java.util.Scanner; // Import the Scanner class
 public class FirstScanner {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in); // Create a Scanner object
+        try {
         System.out.println("Enter a Username");
 
         String userName = myObj.nextLine(); // Read user input
         System.out.println("The Username is: " + userName); // Output user input
+        } finally {
+            in.close();
+        }
     }
 }
